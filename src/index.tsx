ee39c9d5d9d24f24945a172f1e3ff544d914cb08
jakob <jakob.kuczewski@gmail.com>
2022-03-21 21:07:44 +0100

    Changed React Router.

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import {Route, Switch} from 'react-router';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
 
 const jakobsDefaultCategories = [115, 116, 117, 8, 3, 131, 92, 82, 40, 47, 51, 52].map((x: any) => x.toString());
 
